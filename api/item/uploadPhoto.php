494b25a66bae5e7b74fb12fdbed13762863d0692
Вадим Красильников <krasilkoffvad99@gmail.com>
2023-05-28 03:01:28 +0500

    v2.0

--- api/item/uploadPhoto.php
+++ api/item/uploadPhoto.php
@@ -7,40 +7,6 @@ if ($http_origin == "https://apteka-omega.vercel.app" || $http_origin == "http:/
 }
 header("Content-Type: multipart/form-data");
 header("Access-Control-Allow-Methods: POST");
-function can_upload($file){
-	// если имя пустое, значит файл не выбран
-    if($file['name'] == '')
-		return 'Вы не выбрали файл.';
-	
-	/* если размер файла 0, значит его не пропустили настройки 
-	сервера из-за того, что он слишком большой */
-	if($file['size'] == 0)
-		return 'Файл слишком большой.';
-	
-	// разбиваем имя файла по точке и получаем массив
-	$getMime = explode('.', $file['name']);
-	// нас интересует последний элемент массива - расширение
-	$mime = strtolower(end($getMime));
-	// объявим массив допустимых расширений
-	$types = array('jpg', 'png', 'gif', 'bmp', 'jpeg');
-	
-	// если расширение не входит в список допустимых - return
-	if(!in_array($mime, $types))
-		return 'Недопустимый тип файла.';
-	
-	return true;
-  }
-  
-  function make_upload($file){
-    
-
-
-
-
-    copy($file['tmp_name'], $_SERVER['DOCUMENT_ROOT']. '/image/items/' . $name);
-	// move_uploaded_file($file['tmp_name'], 'http://localhost/image/items/' );
-  return  "http://localhost/image/items/" . $name;
-  }
 
 
   // Проверяем, был ли отправлен файл
