528cf807963ffe021152a519b8739ef8e260ce82
Вадим Красильников <krasilkoffvad99@gmail.com>
2023-06-03 15:09:04 +0500

    v2.5

--- api/item/getItems.php
+++ api/item/getItems.php
@@ -8,33 +8,25 @@ if ($http_origin == "https://apteka-omega.vercel.app" || $http_origin == "http:/
 }
 header("Content-Type: application/json; charset=utf-8");
 
-// подключение базы данных и файл, содержащий объекты
 include_once "../config/database.php";
 include_once "../objects/item.php";
 
-// получаем соединение с базой данных
 $database = new Database();
 $db = $database->getConnection();
 
-// инициализируем объект
+
 $item = new Item($db);
  
-// чтение товаров будет здесь
+
 $stmt = $item->get_items();
 $num = $stmt->rowCount();
 
-// проверка, найдено ли больше 0 записей
 if ($num > 0) {
-    // массив товаров
-    $products_arr = array();
-    // $products_arr["response"] =1;
-    // $products_arr["items"] = array();
-   
 
-    // получаем содержимое нашей таблицы
-    // fetch() быстрее, чем fetchAll()
+    $products_arr = array();
+
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        // извлекаем строку
+
         extract($row);
         $product_item = array(
             "id" => $id,
@@ -48,19 +40,14 @@ if ($num > 0) {
         );
         array_push($products_arr, $product_item);
     }
-
-    // устанавливаем код ответа - 200 OK
+    
     http_response_code(200);
-
-    // выводим данные о товаре в формате JSON
+    
     echo json_encode(array("response" => 1, 'items' => $products_arr ), JSON_UNESCAPED_UNICODE);
 }
 
-// "товары не найдены" будет здесь
 else {
-    // установим код ответа - 404 Не найдено
     http_response_code(404);
 
-    // сообщаем пользователю, что товары не найдены
     echo json_encode(array("message" => "Товары не найдены."), JSON_UNESCAPED_UNICODE);
 }
